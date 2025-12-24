f76c8fd7ee6b0b575f44d62397d3cf49e4c38260
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-24 06:52:11 +0000

    Update pictures.lua - Added 51 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -4655,5 +4655,201 @@ return {
     ["naveg6"] = {
         label = "naveg6",
         image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/naveg6.png"
+    },
+    ["c63spd"] = {
+        label = "c63spd",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/c63spd.png"
+    },
+    ["c63wagon"] = {
+        label = "c63wagon",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/c63wagon.png"
+    },
+    ["cls19mafia"] = {
+        label = "cls19mafia",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/cls19mafia.png"
+    },
+    ["clssb"] = {
+        label = "clssb",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/clssb.png"
+    },
+    ["cormeum"] = {
+        label = "cormeum",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/cormeum.png"
+    },
+    ["e63custom18"] = {
+        label = "e63custom18",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/e63custom18.png"
+    },
+    ["e63sc"] = {
+        label = "e63sc",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/e63sc.png"
+    },
+    ["e500"] = {
+        label = "e500",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/e500.png"
+    },
+    ["e632014"] = {
+        label = "e632014",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/e632014.png"
+    },
+    ["fabsls"] = {
+        label = "fabsls",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/fabsls.png"
+    },
+    ["G63BLACK20"] = {
+        label = "G63BLACK20",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/G63BLACK20.png"
+    },
+    ["g63khan"] = {
+        label = "g63khan",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/g63khan.png"
+    },
+    ["G632019"] = {
+        label = "G632019",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/G632019.png"
+    },
+    ["GODzEXY6x6OFFROAD"] = {
+        label = "GODzEXY6x6OFFROAD",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/GODzEXY6x6OFFROAD.png"
+    },
+    ["GODzONYXG63"] = {
+        label = "GODzONYXG63",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/GODzONYXG63.png"
+    },
+    ["gt63beast"] = {
+        label = "gt63beast",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/gt63beast.png"
+    },
+    ["gt63mansory"] = {
+        label = "gt63mansory",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/gt63mansory.png"
+    },
+    ["gtrcf1"] = {
+        label = "gtrcf1",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/gtrcf1.png"
+    },
+    ["lumma"] = {
+        label = "lumma",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/lumma.png"
+    },
+    ["LUMMACLRG800"] = {
+        label = "LUMMACLRG800",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/LUMMACLRG800.png"
+    },
+    ["mansamgt21"] = {
+        label = "mansamgt21",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/mansamgt21.png"
+    },
+    ["manss63"] = {
+        label = "manss63",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/manss63.png"
+    },
+    ["rmodamggt"] = {
+        label = "rmodamggt",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/rmodamggt.png"
+    },
+    ["rrs580"] = {
+        label = "rrs580",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/rrs580.png"
+    },
+    ["rrsls"] = {
+        label = "rrsls",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/rrsls.png"
+    },
+    ["s63c217"] = {
+        label = "s63c217",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/s63c217.png"
+    },
+    ["s63dy"] = {
+        label = "s63dy",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/s63dy.png"
+    },
+    ["s63mafiav2"] = {
+        label = "s63mafiav2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/s63mafiav2.png"
+    },
+    ["s63mansory"] = {
+        label = "s63mansory",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/s63mansory.png"
+    },
+    ["s63msc"] = {
+        label = "s63msc",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/s63msc.png"
+    },
+    ["S632NCS"] = {
+        label = "S632NCS",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/S632NCS.png"
+    },
+    ["safetyamggt63"] = {
+        label = "safetyamggt63",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/safetyamggt63.png"
+    },
+    ["slrsm09"] = {
+        label = "slrsm09",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/slrsm09.png"
+    },
+    ["slsrbs"] = {
+        label = "slsrbs",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/slsrbs.png"
+    },
+    ["vanzbenz"] = {
+        label = "vanzbenz",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/vanzbenz.png"
+    },
+    ["vclass21"] = {
+        label = "vclass21",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/vclass21.png"
+    },
+    ["w126Brabus"] = {
+        label = "w126Brabus",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/w126Brabus.png"
+    },
+    ["x3slr772"] = {
+        label = "x3slr772",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/x3slr772.png"
+    },
+    ["zlay_asap"] = {
+        label = "zlay_asap",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/zlay_asap.png"
+    },
+    ["190e"] = {
+        label = "190e",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/190e.png"
+    },
+    ["190eevo3"] = {
+        label = "190eevo3",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/190eevo3.png"
+    },
+    ["2020CLA45s"] = {
+        label = "2020CLA45s",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/2020CLA45s.png"
+    },
+    ["a45s20"] = {
+        label = "a45s20",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/a45s20.png"
+    },
+    ["amggtc20"] = {
+        label = "amggtc20",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/amggtc20.png"
+    },
+    ["amggtrr"] = {
+        label = "amggtrr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/amggtrr.png"
+    },
+    ["amghr"] = {
+        label = "amghr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/amghr.png"
+    },
+    ["asbo1"] = {
+        label = "asbo1",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/asbo1.png"
+    },
+    ["brabusgtr"] = {
+        label = "brabusgtr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/brabusgtr.png"
+    },
+    ["c63b"] = {
+        label = "c63b",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/MERCEDES/c63b.png"
     }
 }

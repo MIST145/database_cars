35fbceab74fe1a9812e6adb7ff642248cd1fdb80
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-24 08:25:32 +0000

    Add bmw_no_bg_links.lua - Generated links file

---
+++ BMW/no_background/bmw_no_bg_links.lua
@@ -0,0 +1,123 @@
+-- FiveManage Images
+return {
+    ["1m2011"] = {
+        label = "1m2011",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/1m2011.png"
+    },
+    ["2ncsbmwm8"] = {
+        label = "2ncsbmwm8",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/2ncsbmwm8.png"
+    },
+    ["16m4gts"] = {
+        label = "16m4gts",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/16m4gts.png"
+    },
+    ["17m4f82"] = {
+        label = "17m4f82",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/17m4f82.png"
+    },
+    ["18mh5"] = {
+        label = "18mh5",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/18mh5.png"
+    },
+    ["20x5m"] = {
+        label = "20x5m",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/20x5m.png"
+    },
+    ["20xb7"] = {
+        label = "20xb7",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/20xb7.png"
+    },
+    ["22m135i"] = {
+        label = "22m135i",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/22m135i.png"
+    },
+    ["22m135imperf"] = {
+        label = "22m135imperf",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/22m135imperf.png"
+    },
+    ["23m2"] = {
+        label = "23m2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/23m2.png"
+    },
+    ["36cab"] = {
+        label = "36cab",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/36cab.png"
+    },
+    ["640gt"] = {
+        label = "640gt",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/640gt.png"
+    },
+    ["850csi"] = {
+        label = "850csi",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/850csi.png"
+    },
+    ["abflugm2"] = {
+        label = "abflugm2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/abflugm2.png"
+    },
+    ["alpinab7BLACKEDITION"] = {
+        label = "alpinab7BLACKEDITION",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/alpinab7BLACKEDITION.png"
+    },
+    ["BMWM2Manhart"] = {
+        label = "BMWM2Manhart",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/BMWM2Manhart.png"
+    },
+    ["BMWM5"] = {
+        label = "BMWM5",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/BMWM5.png"
+    },
+    ["BMWM5CS"] = {
+        label = "BMWM5CS",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/BMWM5CS.png"
+    },
+    ["BMWM5PRZEMO"] = {
+        label = "BMWM5PRZEMO",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/BMWM5PRZEMO.png"
+    },
+    ["CarsonsM4"] = {
+        label = "CarsonsM4",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/CarsonsM4.png"
+    },
+    ["GC_BMWM5"] = {
+        label = "GC_BMWM5",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GC_BMWM5.png"
+    },
+    ["GODz21M3"] = {
+        label = "GODz21M3",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GODz21M3.png"
+    },
+    ["GODzE46M3LA"] = {
+        label = "GODzE46M3LA",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GODzE46M3LA.png"
+    },
+    ["GODzE46M3LAVERT"] = {
+        label = "GODzE46M3LAVERT",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GODzE46M3LAVERT.png"
+    },
+    ["GODzE46M3PNDM"] = {
+        label = "GODzE46M3PNDM",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GODzE46M3PNDM.png"
+    },
+    ["GODzE46M3PNDMVERT"] = {
+        label = "GODzE46M3PNDMVERT",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GODzE46M3PNDMVERT.png"
+    },
+    ["GODzE46M3VERT"] = {
+        label = "GODzE46M3VERT",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GODzE46M3VERT.png"
+    },
+    ["GODzLBi8"] = {
+        label = "GODzLBi8",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/GODzLBi8.png"
+    },
+    ["M3BXANE"] = {
+        label = "M3BXANE",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/M3BXANE.png"
+    },
+    ["M3F80Vision"] = {
+        label = "M3F80Vision",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/M3F80Vision.png"
+    }
+}

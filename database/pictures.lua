ca3de5aa0dc30a996c506245c3e7c40203be6593
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-24 06:47:49 +0000

    Update pictures.lua - Added 13 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -4607,5 +4607,53 @@ return {
     ["640gt"] = {
         label = "640gt",
         image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/with_background/640gt.jpg"
+    },
+    ["arsx"] = {
+        label = "arsx",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/arsx.png"
+    },
+    ["civicek9rb"] = {
+        label = "civicek9rb",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/civicek9rb.png"
+    },
+    ["ef9"] = {
+        label = "ef9",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/ef9.png"
+    },
+    ["eg6kanjo"] = {
+        label = "eg6kanjo",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/eg6kanjo.png"
+    },
+    ["eg6pandem"] = {
+        label = "eg6pandem",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/eg6pandem.png"
+    },
+    ["eg6stance"] = {
+        label = "eg6stance",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/eg6stance.png"
+    },
+    ["ej2coupe"] = {
+        label = "ej2coupe",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/ej2coupe.png"
+    },
+    ["ek9"] = {
+        label = "ek9",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/ek9.png"
+    },
+    ["ek9kanjo"] = {
+        label = "ek9kanjo",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/ek9kanjo.png"
+    },
+    ["em1coupe"] = {
+        label = "em1coupe",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/em1coupe.png"
+    },
+    ["mrx7"] = {
+        label = "mrx7",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/mrx7.png"
+    },
+    ["naveg6"] = {
+        label = "naveg6",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/HONDA/naveg6.png"
     }
 }

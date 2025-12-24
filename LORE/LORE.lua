59a33f899684495d0a9096e21a8ae6c039eebaa1
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-24 05:46:12 +0000

    Add LORE.lua - Generated links file

---
+++ LORE/LORE.lua
@@ -0,0 +1,63 @@
+-- FiveManage Images
+return {
+    ["m3touring"] = {
+        label = "m3touring",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/m3touring.png"
+    },
+    ["abflugm2"] = {
+        label = "abflugm2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/abflugm2.png"
+    },
+    ["adrom4"] = {
+        label = "adrom4",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/adrom4.png"
+    },
+    ["drem3adro"] = {
+        label = "drem3adro",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/drem3adro.png"
+    },
+    ["e46m3"] = {
+        label = "e46m3",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/e46m3.png"
+    },
+    ["fiestac"] = {
+        label = "fiestac",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/fiestac.png"
+    },
+    ["hycgt500"] = {
+        label = "hycgt500",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/hycgt500.png"
+    },
+    ["m2keyvany"] = {
+        label = "m2keyvany",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/m2keyvany.png"
+    },
+    ["m3hycade"] = {
+        label = "m3hycade",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/m3hycade.png"
+    },
+    ["x6m47"] = {
+        label = "x6m47",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/x6m47.png"
+    },
+    ["rsq3mnsry"] = {
+        label = "rsq3mnsry",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/rsq3mnsry.png"
+    },
+    ["r8novitec"] = {
+        label = "r8novitec",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/r8novitec.png"
+    },
+    ["m4hs"] = {
+        label = "m4hs",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/m4hs.png"
+    },
+    ["m4shark"] = {
+        label = "m4shark",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/m4shark.png"
+    },
+    ["m5abflug"] = {
+        label = "m5abflug",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LORE/m5abflug.png"
+    }
+}

05e09de08d56593bd82088ad5ea7c8d1eff74359
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-29 21:11:01 +0000

    Add LunarCiv_vehicle-image-generator_links.lua - Generated links file

---
+++ LunarCiv/vehicle-image-generator/LunarCiv_vehicle-image-generator_links.lua
@@ -0,0 +1,103 @@
+-- FiveManage Images
+return {
+    ["argento3"] = {
+        label = "argento3",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/argento3.png"
+    },
+    ["cypherest"] = {
+        label = "cypherest",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/cypherest.png"
+    },
+    ["explorer"] = {
+        label = "explorer",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/explorer.png"
+    },
+    ["galatt"] = {
+        label = "galatt",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/galatt.png"
+    },
+    ["galatt2"] = {
+        label = "galatt2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/galatt2.png"
+    },
+    ["gexige"] = {
+        label = "gexige",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/gexige.png"
+    },
+    ["grotti181"] = {
+        label = "grotti181",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/grotti181.png"
+    },
+    ["italiconcept"] = {
+        label = "italiconcept",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/italiconcept.png"
+    },
+    ["italisuv"] = {
+        label = "italisuv",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/italisuv.png"
+    },
+    ["neonsuv"] = {
+        label = "neonsuv",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/neonsuv.png"
+    },
+    ["oraclestd"] = {
+        label = "oraclestd",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/oraclestd.png"
+    },
+    ["oshadow"] = {
+        label = "oshadow",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/oshadow.png"
+    },
+    ["schlagengt4"] = {
+        label = "schlagengt4",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/schlagengt4.png"
+    },
+    ["schwest2"] = {
+        label = "schwest2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/schwest2.png"
+    },
+    ["seeker"] = {
+        label = "seeker",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/seeker.png"
+    },
+    ["sentinelxr"] = {
+        label = "sentinelxr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/sentinelxr.png"
+    },
+    ["serranostr"] = {
+        label = "serranostr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/serranostr.png"
+    },
+    ["shifter"] = {
+        label = "shifter",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/shifter.png"
+    },
+    ["speedojnr"] = {
+        label = "speedojnr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/speedojnr.png"
+    },
+    ["stalker"] = {
+        label = "stalker",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/stalker.png"
+    },
+    ["strafe"] = {
+        label = "strafe",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/strafe.png"
+    },
+    ["strikertour"] = {
+        label = "strikertour",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/strikertour.png"
+    },
+    ["strozzino"] = {
+        label = "strozzino",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/strozzino.png"
+    },
+    ["vulcar90cx"] = {
+        label = "vulcar90cx",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/vulcar90cx.png"
+    },
+    ["wolfr"] = {
+        label = "wolfr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/vehicle-image-generator/wolfr.png"
+    }
+}

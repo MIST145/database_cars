9f9ea9df505a433ea974a33ad93abdba6b04fef4
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-29 21:06:02 +0000

    Update pictures.lua - Added 25 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -4903,5 +4903,101 @@ return {
     ["m4g82p"] = {
         label = "m4g82p",
         image = "https://raw.githubusercontent.com/MIST145/database_cars/main/BMW/no_background/m4g82p.png"
+    },
+    ["argento3"] = {
+        label = "argento3",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/argento3.png"
+    },
+    ["cypherest"] = {
+        label = "cypherest",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/cypherest.png"
+    },
+    ["galatt"] = {
+        label = "galatt",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/galatt.png"
+    },
+    ["galatt2"] = {
+        label = "galatt2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/galatt2.png"
+    },
+    ["gexige"] = {
+        label = "gexige",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/gexige.png"
+    },
+    ["grotti181"] = {
+        label = "grotti181",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/grotti181.png"
+    },
+    ["italiconcept"] = {
+        label = "italiconcept",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/italiconcept.png"
+    },
+    ["italisuv"] = {
+        label = "italisuv",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/italisuv.png"
+    },
+    ["neonsuv"] = {
+        label = "neonsuv",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/neonsuv.png"
+    },
+    ["oraclestd"] = {
+        label = "oraclestd",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/oraclestd.png"
+    },
+    ["oshadow"] = {
+        label = "oshadow",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/oshadow.png"
+    },
+    ["schlagengt4"] = {
+        label = "schlagengt4",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/schlagengt4.png"
+    },
+    ["schwest2"] = {
+        label = "schwest2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/schwest2.png"
+    },
+    ["seeker"] = {
+        label = "seeker",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/seeker.png"
+    },
+    ["sentinelxr"] = {
+        label = "sentinelxr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/sentinelxr.png"
+    },
+    ["serranostr"] = {
+        label = "serranostr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/serranostr.png"
+    },
+    ["shifter"] = {
+        label = "shifter",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/shifter.png"
+    },
+    ["speedojnr"] = {
+        label = "speedojnr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/speedojnr.png"
+    },
+    ["stalker"] = {
+        label = "stalker",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/stalker.png"
+    },
+    ["strafe"] = {
+        label = "strafe",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/strafe.png"
+    },
+    ["strikertour"] = {
+        label = "strikertour",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/strikertour.png"
+    },
+    ["strozzino"] = {
+        label = "strozzino",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/strozzino.png"
+    },
+    ["vulcar90cx"] = {
+        label = "vulcar90cx",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/vulcar90cx.png"
+    },
+    ["wolfr"] = {
+        label = "wolfr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/wolfr.png"
     }
 }

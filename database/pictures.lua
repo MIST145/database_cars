84e6039a5fb09b6ad911e2f9ce2eb29904a118d3
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-30 00:24:10 +0000

    Update pictures.lua - Added 105 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -4999,5 +4999,405 @@ return {
     ["wolfr"] = {
         label = "wolfr",
         image = "https://raw.githubusercontent.com/MIST145/database_cars/main/LunarCiv/greenscreen/wolfr.png"
+    },
+    ["banshee87"] = {
+        label = "banshee87",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/banshee87.png"
+    },
+    ["bs17"] = {
+        label = "bs17",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/bs17.png"
+    },
+    ["GODz14HDSGBAGGER"] = {
+        label = "GODz14HDSGBAGGER",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/GODz14HDSGBAGGER.png"
+    },
+    ["h2carb"] = {
+        label = "h2carb",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/h2carb.png"
+    },
+    ["jambia"] = {
+        label = "jambia",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/jambia.png"
+    },
+    ["tunak"] = {
+        label = "tunak",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/tunak.png"
+    },
+    ["ym1"] = {
+        label = "ym1",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/ym1.png"
+    },
+    ["yr1gsd"] = {
+        label = "yr1gsd",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/yr1gsd.png"
+    },
+    ["yzfr7"] = {
+        label = "yzfr7",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/yzfr7.png"
+    },
+    ["14rnbrt"] = {
+        label = "14rnbrt",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/14rnbrt.png"
+    },
+    ["acknod_cholo"] = {
+        label = "acknod_cholo",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/acknod_cholo.png"
+    },
+    ["acknodlow"] = {
+        label = "acknodlow",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/acknodlow.png"
+    },
+    ["baikal"] = {
+        label = "baikal",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/baikal.png"
+    },
+    ["Barracuda"] = {
+        label = "Barracuda",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/Barracuda.png"
+    },
+    ["BEASTFXRVITO"] = {
+        label = "BEASTFXRVITO",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/BEASTFXRVITO.png"
+    },
+    ["beastrg"] = {
+        label = "beastrg",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/beastrg.png"
+    },
+    ["beater"] = {
+        label = "beater",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/beater.png"
+    },
+    ["beaver"] = {
+        label = "beaver",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/beaver.png"
+    },
+    ["bishop"] = {
+        label = "bishop",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/bishop.png"
+    },
+    ["blackhorse"] = {
+        label = "blackhorse",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/blackhorse.png"
+    },
+    ["bluestar"] = {
+        label = "bluestar",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/bluestar.png"
+    },
+    ["born"] = {
+        label = "born",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/born.png"
+    },
+    ["borngrim"] = {
+        label = "borngrim",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/borngrim.png"
+    },
+    ["brat2"] = {
+        label = "brat2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/brat2.png"
+    },
+    ["bulldog"] = {
+        label = "bulldog",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/bulldog.png"
+    },
+    ["carabelachris"] = {
+        label = "carabelachris",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/carabelachris.png"
+    },
+    ["chibs"] = {
+        label = "chibs",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/chibs.png"
+    },
+    ["chingon"] = {
+        label = "chingon",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/chingon.png"
+    },
+    ["clay"] = {
+        label = "clay",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/clay.png"
+    },
+    ["clubdyna33"] = {
+        label = "clubdyna33",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/clubdyna33.png"
+    },
+    ["clubsg"] = {
+        label = "clubsg",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/clubsg.png"
+    },
+    ["Crimea"] = {
+        label = "Crimea",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/Crimea.png"
+    },
+    ["cvo"] = {
+        label = "cvo",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/cvo.png"
+    },
+    ["dagger"] = {
+        label = "dagger",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/dagger.png"
+    },
+    ["delaware"] = {
+        label = "delaware",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/delaware.png"
+    },
+    ["delgado"] = {
+        label = "delgado",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/delgado.png"
+    },
+    ["dozer"] = {
+        label = "dozer",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/dozer.png"
+    },
+    ["dyna"] = {
+        label = "dyna",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/dyna.png"
+    },
+    ["dyne"] = {
+        label = "dyne",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/dyne.png"
+    },
+    ["ebagger"] = {
+        label = "ebagger",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/ebagger.png"
+    },
+    ["foxharley2"] = {
+        label = "foxharley2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/foxharley2.png"
+    },
+    ["happy"] = {
+        label = "happy",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/happy.png"
+    },
+    ["harrier"] = {
+        label = "harrier",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/harrier.png"
+    },
+    ["hidalgoI"] = {
+        label = "hidalgoI",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/hidalgoI.png"
+    },
+    ["hidalgoII"] = {
+        label = "hidalgoII",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/hidalgoII.png"
+    },
+    ["hussar"] = {
+        label = "hussar",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/hussar.png"
+    },
+    ["jax2"] = {
+        label = "jax2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/jax2.png"
+    },
+    ["lst3"] = {
+        label = "lst3",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/lst3.png"
+    },
+    ["LV"] = {
+        label = "LV",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/LV.png"
+    },
+    ["maya"] = {
+        label = "maya",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/maya.png"
+    },
+    ["memory"] = {
+        label = "memory",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/memory.png"
+    },
+    ["na25"] = {
+        label = "na25",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/na25.png"
+    },
+    ["nocturnoII"] = {
+        label = "nocturnoII",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/nocturnoII.png"
+    },
+    ["offlrds1000r"] = {
+        label = "offlrds1000r",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/offlrds1000r.png"
+    },
+    ["opie"] = {
+        label = "opie",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/opie.png"
+    },
+    ["pathfinder"] = {
+        label = "pathfinder",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/pathfinder.png"
+    },
+    ["redstar"] = {
+        label = "redstar",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/redstar.png"
+    },
+    ["reitar"] = {
+        label = "reitar",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/reitar.png"
+    },
+    ["reitarII"] = {
+        label = "reitarII",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/reitarII.png"
+    },
+    ["roadbag"] = {
+        label = "roadbag",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/roadbag.png"
+    },
+    ["roadglide"] = {
+        label = "roadglide",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/roadglide.png"
+    },
+    ["softail1"] = {
+        label = "softail1",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/softail1.png"
+    },
+    ["springer"] = {
+        label = "springer",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/springer.png"
+    },
+    ["trapper2"] = {
+        label = "trapper2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/trapper2.png"
+    },
+    ["zx10r22"] = {
+        label = "zx10r22",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/zx10r22.png"
+    },
+    ["devotee"] = {
+        label = "devotee",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/devotee.png"
+    },
+    ["dinorex"] = {
+        label = "dinorex",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/dinorex.png"
+    },
+    ["dragoon"] = {
+        label = "dragoon",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/dragoon.png"
+    },
+    ["duke"] = {
+        label = "duke",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/duke.png"
+    },
+    ["dynals"] = {
+        label = "dynals",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/dynals.png"
+    },
+    ["eastwood"] = {
+        label = "eastwood",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/eastwood.png"
+    },
+    ["enforcer"] = {
+        label = "enforcer",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/enforcer.png"
+    },
+    ["Esquivo"] = {
+        label = "Esquivo",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/Esquivo.png"
+    },
+    ["fatboy21"] = {
+        label = "fatboy21",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/fatboy21.png"
+    },
+    ["gargoyle"] = {
+        label = "gargoyle",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/gargoyle.png"
+    },
+    ["hamster"] = {
+        label = "hamster",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/hamster.png"
+    },
+    ["hdglide2027"] = {
+        label = "hdglide2027",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/hdglide2027.png"
+    },
+    ["hellhound"] = {
+        label = "hellhound",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/hellhound.png"
+    },
+    ["hollerboy"] = {
+        label = "hollerboy",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/hollerboy.png"
+    },
+    ["inquisitor"] = {
+        label = "inquisitor",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/inquisitor.png"
+    },
+    ["Jango"] = {
+        label = "Jango",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/Jango.png"
+    },
+    ["jayteepror"] = {
+        label = "jayteepror",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/jayteepror.png"
+    },
+    ["Jetlag"] = {
+        label = "Jetlag",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/Jetlag.png"
+    },
+    ["lowst"] = {
+        label = "lowst",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/lowst.png"
+    },
+    ["MUNSON"] = {
+        label = "MUNSON",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/MUNSON.png"
+    },
+    ["okinawa"] = {
+        label = "okinawa",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/okinawa.png"
+    },
+    ["perfsporty"] = {
+        label = "perfsporty",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/perfsporty.png"
+    },
+    ["pglide"] = {
+        label = "pglide",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/pglide.png"
+    },
+    ["pike"] = {
+        label = "pike",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/pike.png"
+    },
+    ["powmia"] = {
+        label = "powmia",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/powmia.png"
+    },
+    ["Rapido"] = {
+        label = "Rapido",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/Rapido.png"
+    },
+    ["rass"] = {
+        label = "rass",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/rass.png"
+    },
+    ["rd"] = {
+        label = "rd",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/rd.png"
+    },
+    ["rdg"] = {
+        label = "rdg",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/rdg.png"
+    },
+    ["reaper1"] = {
+        label = "reaper1",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/reaper1.png"
+    },
+    ["rgs22turbo"] = {
+        label = "rgs22turbo",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/rgs22turbo.png"
+    },
+    ["rider"] = {
+        label = "rider",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/rider.png"
+    },
+    ["RK2019A"] = {
+        label = "RK2019A",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/RK2019A.png"
+    },
+    ["sa90"] = {
+        label = "sa90",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/sa90.png"
+    },
+    ["skat"] = {
+        label = "skat",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/skat.png"
     }
 }

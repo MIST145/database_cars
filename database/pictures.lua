bb136243968fcf227769c9fa2ad8c6d7d844b7b3
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-30 00:49:09 +0000

    Update pictures.lua - Added 4 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -5399,5 +5399,21 @@ return {
     ["skat"] = {
         label = "skat",
         image = "https://raw.githubusercontent.com/MIST145/database_cars/main/hs_bikes/vehicle-image-generator/skat.png"
+    },
+    ["imp_prop_cookie_lift"] = {
+        label = "imp_prop_cookie_lift",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/props_lifts/imp_prop_cookie_lift.png"
+    },
+    ["imp_prop_kq_lift"] = {
+        label = "imp_prop_kq_lift",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/props_lifts/imp_prop_kq_lift.png"
+    },
+    ["okok_car_lift"] = {
+        label = "okok_car_lift",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/props_lifts/okok_car_lift.png"
+    },
+    ["t1ger_carlift"] = {
+        label = "t1ger_carlift",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/props_lifts/t1ger_carlift.png"
     }
 }

de9954bb580782ff170a092b03428c12cf81acfb
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-31 22:43:39 +0000

    Add GAME1_2-links.lua - Generated links file

---
+++ GAME1_2-links.lua
@@ -0,0 +1,371 @@
+-- FiveManage Images
+return {
+    ["lp700r"] = {
+        label = "lp700r",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/lp700r.png"
+    },
+    ["lrrr"] = {
+        label = "lrrr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/lrrr.png"
+    },
+    ["lykan"] = {
+        label = "lykan",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/lykan.png"
+    },
+    ["m2"] = {
+        label = "m2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/m2.png"
+    },
+    ["m3e36"] = {
+        label = "m3e36",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/m3e36.png"
+    },
+    ["m3e92"] = {
+        label = "m3e92",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/m3e92.png"
+    },
+    ["m3f80"] = {
+        label = "m3f80",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/m3f80.png"
+    },
+    ["m4f82"] = {
+        label = "m4f82",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/m4f82.png"
+    },
+    ["m6f13"] = {
+        label = "m6f13",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/m6f13.png"
+    },
+    ["maj350"] = {
+        label = "maj350",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/maj350.png"
+    },
+    ["maj935"] = {
+        label = "maj935",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/maj935.png"
+    },
+    ["majfd"] = {
+        label = "majfd",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/majfd.png"
+    },
+    ["mbc63"] = {
+        label = "mbc63",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/mbc63.png"
+    },
+    ["mcst"] = {
+        label = "mcst",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/mcst.png"
+    },
+    ["miata3"] = {
+        label = "miata3",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/miata3.png"
+    },
+    ["mig"] = {
+        label = "mig",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/mig.png"
+    },
+    ["mk2100"] = {
+        label = "mk2100",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/mk2100.png"
+    },
+    ["models"] = {
+        label = "models",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/models.png"
+    },
+    ["mp412c"] = {
+        label = "mp412c",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/mp412c.png"
+    },
+    ["mustang50th"] = {
+        label = "mustang50th",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/mustang50th.png"
+    },
+    ["mustang2005gt"] = {
+        label = "mustang2005gt",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/mustang2005gt.png"
+    },
+    ["nis15"] = {
+        label = "nis15",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/nis15.png"
+    },
+    ["nissantitan17"] = {
+        label = "nissantitan17",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/nissantitan17.png"
+    },
+    ["ns350"] = {
+        label = "ns350",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/ns350.png"
+    },
+    ["nzp"] = {
+        label = "nzp",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/nzp.png"
+    },
+    ["p90d"] = {
+        label = "p90d",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/p90d.png"
+    },
+    ["passat"] = {
+        label = "passat",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/passat.png"
+    },
+    ["pcs18"] = {
+        label = "pcs18",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/pcs18.png"
+    },
+    ["pm19"] = {
+        label = "pm19",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/pm19.png"
+    },
+    ["q820"] = {
+        label = "q820",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/q820.png"
+    },
+    ["r8ppi"] = {
+        label = "r8ppi",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/r8ppi.png"
+    },
+    ["raid"] = {
+        label = "raid",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/raid.png"
+    },
+    ["ram2500"] = {
+        label = "ram2500",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/ram2500.png"
+    },
+    ["raptor2017"] = {
+        label = "raptor2017",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/raptor2017.png"
+    },
+    ["rcf"] = {
+        label = "rcf",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/rcf.png"
+    },
+    ["rculi"] = {
+        label = "rculi",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/rculi.png"
+    },
+    ["regalia"] = {
+        label = "regalia",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/regalia.png"
+    },
+    ["regera"] = {
+        label = "regera",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/regera.png"
+    },
+    ["rrevoque"] = {
+        label = "rrevoque",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/rrevoque.png"
+    },
+    ["rrphantom"] = {
+        label = "rrphantom",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/rrphantom.png"
+    },
+    ["rrst"] = {
+        label = "rrst",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/rrst.png"
+    },
+    ["rsvr16"] = {
+        label = "rsvr16",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/rsvr16.png"
+    },
+    ["s8d2"] = {
+        label = "s8d2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/s8d2.png"
+    },
+    ["s500w222"] = {
+        label = "s500w222",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/s500w222.png"
+    },
+    ["sandero"] = {
+        label = "sandero",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/sandero.png"
+    },
+    ["senna"] = {
+        label = "senna",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/senna.png"
+    },
+    ["sl500"] = {
+        label = "sl500",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/sl500.png"
+    },
+    ["sq72016"] = {
+        label = "sq72016",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/sq72016.png"
+    },
+    ["srt4"] = {
+        label = "srt4",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/srt4.png"
+    },
+    ["srt8"] = {
+        label = "srt8",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/srt8.png"
+    },
+    ["srt8b"] = {
+        label = "srt8b",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/srt8b.png"
+    },
+    ["stepway"] = {
+        label = "stepway",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/stepway.png"
+    },
+    ["stingray"] = {
+        label = "stingray",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/stingray.png"
+    },
+    ["subisti08"] = {
+        label = "subisti08",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/subisti08.png"
+    },
+    ["subwrx"] = {
+        label = "subwrx",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/subwrx.png"
+    },
+    ["svj63"] = {
+        label = "svj63",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/svj63.png"
+    },
+    ["svx"] = {
+        label = "svx",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/svx.png"
+    },
+    ["tahoe21"] = {
+        label = "tahoe21",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/tahoe21.png"
+    },
+    ["taycan"] = {
+        label = "taycan",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/taycan.png"
+    },
+    ["teslapd"] = {
+        label = "teslapd",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/teslapd.png"
+    },
+    ["teslax"] = {
+        label = "teslax",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/teslax.png"
+    },
+    ["tltypes"] = {
+        label = "tltypes",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/tltypes.png"
+    },
+    ["tmodel"] = {
+        label = "tmodel",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/tmodel.png"
+    },
+    ["toysupmk4"] = {
+        label = "toysupmk4",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/toysupmk4.png"
+    },
+    ["tr22"] = {
+        label = "tr22",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/tr22.png"
+    },
+    ["trhawk"] = {
+        label = "trhawk",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/trhawk.png"
+    },
+    ["trx"] = {
+        label = "trx",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/trx.png"
+    },
+    ["ttrs"] = {
+        label = "ttrs",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/ttrs.png"
+    },
+    ["twingo"] = {
+        label = "twingo",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/twingo.png"
+    },
+    ["urus"] = {
+        label = "urus",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/urus.png"
+    },
+    ["v250"] = {
+        label = "v250",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/v250.png"
+    },
+    ["veneno"] = {
+        label = "veneno",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/veneno.png"
+    },
+    ["vwr"] = {
+        label = "vwr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/vwr.png"
+    },
+    ["vxr"] = {
+        label = "vxr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/vxr.png"
+    },
+    ["wildtrak"] = {
+        label = "wildtrak",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/wildtrak.png"
+    },
+    ["wmfenyr"] = {
+        label = "wmfenyr",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/wmfenyr.png"
+    },
+    ["wraith"] = {
+        label = "wraith",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/wraith.png"
+    },
+    ["x5e53"] = {
+        label = "x5e53",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/x5e53.png"
+    },
+    ["x6m"] = {
+        label = "x6m",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/x6m.png"
+    },
+    ["xc90"] = {
+        label = "xc90",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/xc90.png"
+    },
+    ["yFe458i1"] = {
+        label = "yFe458i1",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFe458i1.png"
+    },
+    ["yFe458i2"] = {
+        label = "yFe458i2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFe458i2.png"
+    },
+    ["yFe458s1"] = {
+        label = "yFe458s1",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFe458s1.png"
+    },
+    ["yFe458s1X"] = {
+        label = "yFe458s1X",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFe458s1X.png"
+    },
+    ["yFe458s2"] = {
+        label = "yFe458s2",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFe458s2.png"
+    },
+    ["yFe458s2X"] = {
+        label = "yFe458s2X",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFe458s2X.png"
+    },
+    ["yFeF12A"] = {
+        label = "yFeF12A",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFeF12A.png"
+    },
+    ["yFeF12T"] = {
+        label = "yFeF12T",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yFeF12T.png"
+    },
+    ["yPG205t16A"] = {
+        label = "yPG205t16A",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yPG205t16A.png"
+    },
+    ["yPG205t16B"] = {
+        label = "yPG205t16B",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/yPG205t16B.png"
+    },
+    ["z32"] = {
+        label = "z32",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/z32.png"
+    },
+    ["z419"] = {
+        label = "z419",
+        image = "https://raw.githubusercontent.com/MIST145/database_cars/main/game/z419.png"
+    }
+}
